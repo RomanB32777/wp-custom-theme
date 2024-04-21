afffcb022ae7d7f35351f20c17af1b82b095d636
Roman Bogdanovskiy <romain.bogdanovsky@yandex.ru>
2024-04-21 10:48:56 +0300

    feat: added default single file

---
+++ theme-parts/posts/single/default.php
@@ -0,0 +1,43 @@
+<main class="pt-20 pb-10">
+
+	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+		<?php get_template_part( '/theme-parts/breadcrumbs' ); ?>
+	</div>
+
+	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+		<div class="main-blocks [&>*]:my-7 [&>*]:md:!my-14">
+			<?php 
+			if ( have_posts() ) :
+				while ( have_posts() ) :
+					the_post();
+					the_content();
+				endwhile;
+			endif; 
+			?>
+		</div>
+
+		<div class="[&>*]:my-7 [&>*]:md:!my-14">
+
+			<!-- Author Info Start -->
+
+			<?php
+				get_template_part( '/theme-parts/author-info' );
+				get_author_info( get_the_author_meta( 'ID' ), esc_html__( 'Author', 'custom-theme' ), 40, false );
+			?>
+
+			<!-- Author Info End -->
+
+			<!-- Comments Start -->
+
+			<?php
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+			?>
+
+			<!-- Comments End -->
+
+		</div>
+	</div>
+
+</main>
